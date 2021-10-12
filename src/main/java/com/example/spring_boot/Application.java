69120a03854ff8ad61ca9dc7e960f546c2589750
seongwoo-choi <how0326@naver.com>
2021-10-12 10:39:58 +0900

    비동기 처리하는 법

--- src/main/java/com/example/spring_boot/Application.java
+++ src/main/java/com/example/spring_boot/Application.java
@@ -3,9 +3,11 @@ package com.example.spring_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ServletComponentScan // Filter 를 검색해주는 어노테이션
+@EnableAsync
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/spring_boot/controller/ApiController.java
+++ src/main/java/com/example/spring_boot/controller/ApiController.java
@@ -2,20 +2,12 @@ package com.example.spring_boot.controller;
 
 
 import com.example.spring_boot.dto.User;
+import com.example.spring_boot.service.AsyncService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import java.util.concurrent.CompletableFuture;
 
 // lombok 을 사용하면 Slf4j 사용 가능 => log.info() 로 정보를 찍을 수 있다.
 @Slf4j
@@ -23,11 +15,23 @@ import javax.validation.constraints.Size;
 @RequestMapping("/api")
 public class ApiController {
 
+    private final AsyncService asyncService;
+
+    public ApiController(AsyncService asyncService) {
+        this.asyncService = asyncService;
+    }
+
     @PostMapping("/user")
-    public User user(@RequestBody User user){
+    public User user(@RequestBody User user) {
         log.info("user : {}, {}", user, user);
 
         return user;
+    }
+
+    @GetMapping("/hello1")
+    public CompletableFuture hello() {
+        log.info("completable future init");
+        return asyncService.run();
     }
 
 }
